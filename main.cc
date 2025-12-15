0d2e2a878a5140221f9a88b7655c7e3d083c0d1f
msakuta <masahiro.sakuta@gmail.com>
2025-12-15 22:58:53 +0900

    Parse Tree name

--- main.cc
+++ main.cc
@@ -3,7 +3,7 @@
 #include <vector>
 #include "parser.h"
 
-int main() {
+void test_identifier() {
 	std::string src = "Hello world lorem ipsum some_thing\n";
 
 	std::vector<std::string_view> tokens;
@@ -13,12 +13,12 @@ int main() {
 	while (!cursor.empty()) {
 		auto res = identifier(cursor);
 
-		if (std::holds_alternative<std::string>(res)) {
-			std::cout << "Error: " << std::get<std::string>(res) << "\n";
+		if (auto e = std::get_if<1>(&res)) {
+			std::cout << "Error: " << *e << "\n";
 			break;
 		}
 		else {
-			auto pair = std::get<Res>(res);
+			auto pair = std::get<Res<std::string_view>>(res);
 			print_res(pair);
 			tokens.push_back(pair.second);
 			cursor = pair.first;
@@ -29,6 +29,24 @@ int main() {
 	for (auto& token : tokens) {
 		std::cout << token << "\n";
 	}
+}
+
+void test_tree() {
+	std::string src = "tree main = {}";
+
+	auto res = source_text(src);
+
+	if (auto e = std::get_if<1>(&res)) {
+		std::cout << "Error: " << *e << "\n";
+		return;
+	}
+
+	auto pair = std::get<0>(res);
+	std::cout << "Tree parsed: " << pair.second << ", remainder: \"" << pair.first << "\"\n";
+}
+
+int main() {
+	test_tree();
 	return 0;
 }
 
--- parser.h
+++ parser.h
@@ -4,11 +4,13 @@
 struct Node {
 };
 
-using Res = std::pair<std::string_view, std::string_view>;
+template<typename T = std::string_view>
+using Res = std::pair<std::string_view, T>;
 
-using IResult = std::variant<Res, std::string>;
+template<typename T = std::string_view>
+using IResult = std::variant<Res<T>, std::string>;
 
-IResult identifier(std::string_view i) {
+IResult<std::string_view> identifier(std::string_view i) {
 	while (!i.empty() && isspace(i[0])) {
 		i = i.substr(1);
 	}
@@ -21,11 +23,42 @@ IResult identifier(std::string_view i) {
 	while (!r.empty() && (isalnum(r[0]) || r[0] == '_')) {
 		r = r.substr(1);
 	}
-	IResult ret = std::make_pair(r, i.substr(0, r.data() - i.data()));
+	IResult<std::string_view> ret = std::make_pair(r, i.substr(0, r.data() - i.data()));
 	return ret;
 }
 
-void print_res(const Res& res) {
+void print_res(const Res<std::string_view>& res) {
 	std::cout << "(" << res.first << ", " << res.second << ")";
+}
+
+struct Tree {
+	std::string name;
+};
+
+inline std::ostream &operator<<(std::ostream& os, const Tree& tree) {
+	os << "Tree { .name = \"" << tree.name << "\" }";
+	return os;
+}
+
+IResult<Tree> source_text(std::string_view i) {
+	auto res = identifier(i);
+	if (auto e = std::get_if<1>(&res)) {
+		return std::string("Did not recognize the first identifier: " + *e);
+	}
+
+	auto r = std::get<0>(res);
+	if (r.second != "tree") {
+		return std::string("The first identifier must be \"tree\"");
+	}
+
+	auto res2 = identifier(r.first);
+	if (auto e = std::get_if<1>(&res2)) {
+		return std::string("Missing tree name: " + *e);
+	}
+
+	auto r2 = std::get<0>(res2);
+
+
+	return std::make_pair(r2.first, Tree{ .name = std::string(r2.second) });
 }
 
